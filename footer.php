ca45d1039eb3ca770c506f1a1dd6e11ea0113fe8
MinnnErrr <lohminer22@gmail.com>
2024-12-20 13:54:28 +0800

    sideBar renamed to adminSideBar. Update template and elements in the template. Use this latest template

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
-<div class="row bg-light border-top py-2">
-    <div class="col d-flex justify-content-center align-items-center">
+<div class="row bg-body border-top py-2">
+    <footer class="col d-flex justify-content-center align-items-center">
         <a href="#" class="text-decoration-none me-2">
             <img src="./RapidPrintIcon.png" alt="RapidPrint" width="25">
         </a>
         <span>&copy 2024 RapidPrint. All rights reserved.</span>
-    </div>
+    </footer>
 </div>
--- navbar.php
+++ navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar sticky-top shadow-sm bg-white p-2">
+<nav class="navbar sticky-top shadow-sm bg-body p-2">
     <div class="container-fluid">
         <button class="btn d-lg-none" data-bs-toggle="offcanvas" data-bs-target="#offcanvasResponsive" aria-controls="offcanvasResponsive">
             <span class="bi bi-list fs-3"></span>
@@ -7,6 +7,5 @@
             <img class="me-1" src="./RapidPrintIcon.png" alt="RapidPrint" width="30">
             <span class="ms-1">RapidPrint</span>
         </a>
-
     </div>
 </nav>
