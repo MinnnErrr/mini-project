b32faf05a5217a10a28f10a1545a388db581c011
MinnnErrr <lohminer22@gmail.com>
2024-12-20 13:59:01 +0800

    use this latest template

--- template.php
+++ template.php
@@ -18,7 +18,8 @@ if (!isset($user_id)) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Dashboard</title>
+    <!--your page title-->
+    <title></title> 
     <link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="./node_modules/bootstrap-icons/font/bootstrap-icons.css">
     <link rel="stylesheet" href="./main.css">
