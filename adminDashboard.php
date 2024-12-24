3cd380397b5bf00b0a690e53f2040297f008c507
MinnnErrr <lohminer22@gmail.com>
2024-12-24 11:00:29 +0800

    update admin

--- adminDashboard.php
+++ adminDashboard.php
@@ -35,7 +35,7 @@ if (!isset($user_id)) {
 
             <!--right content-->
             <div class="col-sm-12 col-lg-10">
-                <div class="container min-vh-100 p-4">
+                <div class="container-fluid min-vh-100 p-4">
 
                     <!--first row-->
                     <div class="row align-items-center mb-4">
@@ -55,28 +55,28 @@ if (!isset($user_id)) {
 
                     <!--second row-->
                     <div class="row">
-                        <div class="col-lg-3 col-sm-12 mb-4 d-lg-flex align-self-stretch">
+                        <div class="col-lg-3 col-sm-12 mb-4">
                             <div class="rounded-3 p-4 bg-white shadow-sm">
                                 <h6>Total Number of Staff</h6>
                                 <p>Number</p>
                             </div>
                         </div>
 
-                        <div class="col-lg-3 col-sm-12 mb-4 d-lg-flex align-self-stretch">
+                        <div class="col-lg-3 col-sm-12 mb-4">
                             <div class="rounded-3 p-4 bg-white shadow-sm">
                                 <h6>Total Number of Customer</h6>
                                 <p>Number</p>
                             </div>
                         </div>
 
-                        <div class="col-lg-3 col-sm-12 mb-4 d-lg-flex align-self-stretch">
+                        <div class="col-lg-3 col-sm-12 mb-4">
                             <div class="rounded-3 p-4 bg-white shadow-sm">
                                 <h6>Total Number of Registered Customer</h6>
                                 <p>Number</p>
                             </div>
                         </div>
 
-                        <div class="col-lg-3 col-sm-12 mb-4 d-lg-flex align-self-stretch">
+                        <div class="col-lg-3 col-sm-12 mb-4">
                             <div class="rounded-3 p-4 bg-white shadow-sm">
                                 <h6>Total Number of Verified Customer</h6>
                                 <p>Number</p>
@@ -87,14 +87,20 @@ if (!isset($user_id)) {
                     <!--graph 1-->
                     <div class="row-lg-12 mb-4">
                         <div class="rounded-3 p-4 bg-white shadow-sm">
-                            graph
+                            <h6>Total Sales based on package</h6>
+                            <div class="d-flex justify-content-center">
+                                <canvas id="chart1" style="width:100%;max-width:800px"></canvas>
+                            </div>
                         </div>
                     </div>
 
                     <!--graph 2-->
                     <div class="row-lg-12">
                         <div class="rounded-3 p-4 bg-white shadow-sm">
-                            graph
+                        <h6>Total Sales based on branch</h6>
+                            <div class="d-flex justify-content-center">
+                                <canvas id="chart2" style="width:100%;max-width:800px"></canvas>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -104,9 +110,72 @@ if (!isset($user_id)) {
         </div>
     </div>
 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
     <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+
+    <!-- for active nav item -->
     <script>
         document.getElementById('dashboard').classList.add('is-active');
+    </script>
+
+    <!-- for charts -->
+    <script>
+        const xValues = [50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150];
+        const yValues = [7, 8, 8, 9, 9, 9, 10, 11, 14, 14, 15];
+
+        new Chart("chart1", {
+            type: "line",
+            data: {
+                labels: xValues,
+                datasets: [{
+                    fill: false,
+                    lineTension: 0,
+                    backgroundColor: "rgba(0,0,255,1.0)",
+                    borderColor: "rgba(0,0,255,0.1)",
+                    data: yValues
+                }]
+            },
+            options: {
+                legend: {
+                    display: false
+                },
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            min: 6,
+                            max: 16
+                        }
+                    }],
+                }
+            }
+        });
+
+        new Chart("chart2", {
+            type: "line",
+            data: {
+                labels: xValues,
+                datasets: [{
+                    fill: false,
+                    lineTension: 0,
+                    backgroundColor: "rgba(0,0,255,1.0)",
+                    borderColor: "rgba(0,0,255,0.1)",
+                    data: yValues
+                }]
+            },
+            options: {
+                legend: {
+                    display: false
+                },
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            min: 6,
+                            max: 16
+                        }
+                    }],
+                }
+            }
+        });
     </script>
 </body>
 
