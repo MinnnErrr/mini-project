44d67b942ac6e2d88a84f3bab7c0c05bdd6e637e
MinnnErrr <lohminer22@gmail.com>
2025-01-07 12:13:31 +0800

    updated admin pages

--- addPackage.php
+++ addPackage.php
@@ -21,9 +21,6 @@ if (!isset($user_id)) {
     <link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="./node_modules/bootstrap-icons/font/bootstrap-icons.css">
     <link rel="stylesheet" href="./main.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
 </head>
 
 
