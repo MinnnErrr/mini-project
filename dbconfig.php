cf8c8bb2e4df7de8f076b1984435e64b85584bf9
MinnnErrr <lohminer22@gmail.com>
2025-01-07 14:08:37 +0800

    update

--- dbconfig.php
+++ dbconfig.php
@@ -4,7 +4,11 @@ $db_name = "mysql:host=localhost;dbname=mini_project";
 $username = "root";
 $password = "";
 
-$conn = new PDO($db_name, $username, $password);
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+try {
+    $conn = new PDO($db_name, $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
 
 ?>
